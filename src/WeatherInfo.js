6fa0a313d744e0778de4706bf8110893ee1b7928
ailin-werner <43426294+ailin-werner@users.noreply.github.com>
2021-02-25 08:29:36 +1100

    Updated the css so it looks good on mobile

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,16 +2,18 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
+import "./weatherinfo.css";
+
 export default function WeatherInfo(props) {
     return (
-            <div className="row padding80">
-                <div className="col">
+            <div className="row d-flex align-items-center paddingTopBottom80">
+                <div className="col-sm">
                     <WeatherTemperature celsius={props.data} celsiusMinTemp={props.data.minTemp} celsiusMaxTemp={props.data.maxTemp} celsiusFeelsLike={props.data.feelsLike} unit={props.unit} setUnit={props.setUnit} />
                 </div>
-                <div className="col">
+                <div className="col-sm">
                     <WeatherIcon code={props.data.icon} />
                 </div>
-                <div className="col">
+                <div className="col-sm">
                     <p className="text-capitalize">{props.data.description}</p>
                     <p>💧 Humidity: {props.data.humidity}%</p>
                     <p>🌬 Wind: {Math.round(props.data.wind)} km/h</p>
